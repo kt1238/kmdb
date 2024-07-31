5a8fa47c7ad458f0935b055affbc226f635bcb6c
kt1238 <kevinthomaskt1984@gmail.com>
2024-07-31 15:44:04 +0530

    updated settings

--- kmdb/settings.py
+++ kmdb/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "j_ipe74)d(a$%e2c^-vbf3tys#cs(nd9b!gqb_m)gc)34$rtaw"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
