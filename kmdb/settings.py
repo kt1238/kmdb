5b07e79a9cd326638aa19fbd9cfb5bc62f996f21
kt1238 <kevinthomaskt1984@gmail.com>
2024-07-31 06:40:01 +0530

    added secret key literal

--- kmdb/settings.py
+++ kmdb/settings.py
@@ -20,8 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open("C:/Users/Kevin/Desktop/Websites/DjangoKeys/kmdb.txt") as f:
-    SECRET_KEY = f.read().strip()
+# with open("C:/Users/Kevin/Desktop/Websites/DjangoKeys/kmdb.txt") as f:
+#     SECRET_KEY = f.read().strip()
+SECRET_KEY = "j_ipe74)d(a$%e2c^-vbf3tys#cs(nd9b!gqb_m)gc)34$rtaw"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
