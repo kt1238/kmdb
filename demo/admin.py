63e8f2c730d717c7b6b1285242f592b3e970b3b1
kt1238 <kevinthomaskt1984@gmail.com>
2024-08-10 19:45:35 +0530

    add search to admin page

--- demo/admin.py
+++ demo/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from demo.models import Movie
 
-# Register your models here.
-admin.site.register(Movie)
+# Creating modified admin class
+class DemoAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+
+# Register your models and new admin class here.
+admin.site.register(Movie, DemoAdmin)
