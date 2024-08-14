515834fb9e344e4f7b0acc0644f5420cef650e12
kt1238 <kevinthomaskt1984@gmail.com>
2024-08-14 18:57:34 +0530

    whitespace fix

--- demo/views.py
+++ demo/views.py
@@ -4,23 +4,27 @@ from load import loadcsvfile
 from demo.models import Movie
 import random
 
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
+
 def all(request):
     movies = Movie.objects.all()
     context = {
-        'movies':movies
+        'movies': movies
     }
-    return render(request,'all.html',context)
+    return render(request, 'all.html', context)
 
-def details(request,id):
-    movie = Movie.objects.get(id = id)
+
+def details(request, id):
+    movie = Movie.objects.get(id=id)
     context = {
         'movie': movie
     }
-    return render(request,'details.html',context)
+    return render(request, 'details.html', context)
+
 
 def search(request):
     if request.method == 'POST':
@@ -30,14 +34,16 @@ def search(request):
     if search_term.isnumeric():
         year_query = Movie.objects.filter(release_year=search_term)
 
-    result = Movie.objects.filter(name__contains=search_term) | Movie.objects.filter(genre_1__contains=search_term) | year_query
+    result = Movie.objects.filter(name__contains=search_term) | Movie.objects.filter(
+        genre_1__contains=search_term) | year_query
 
     context = {
         'search_term': search_term,
         'result': result
     }
-    return render(request,'search.html',context)
+    return render(request, 'search.html', context)
+
 
 def random_movie(request):
-    id = random.randint(1,Movie.objects.count())
-    return details(request,id)
+    id = random.randint(1, Movie.objects.count())
+    return details(request, id)
